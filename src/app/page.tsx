aefba17e52fac4cd808028369d6b9b17dd97d34e
naderabdullah <nader.abdullah@outlook.com>
2025-05-09 23:12:55 -0400

    Removing install banner (temporary)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -80,7 +80,7 @@ export default function Home() {
   return (
     <main className={`container mx-auto px-4 py-6 max-w-4xl ${appReady ? 'app-loading' : 'opacity-0'}`}>
       <OfflineIndicator />
-      <InstallBanner />
+      {/* <InstallBanner /> */}
       
       <Header />
       
