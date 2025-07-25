9d03890380725f6d56ac2b9cbcfa1d8d7800cfd4
naderabdullah <nader.abdullah@outlook.com>
2025-07-25 03:07:54 -0400

    Fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,7 +39,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover, maximum-scale=1, user-scalable=no" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="Logo Gen" />
