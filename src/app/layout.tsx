6d284ff6d00725b7af96b5835d84228cc408f052
naderabdullah <nader.abdullah@outlook.com>
2025-07-25 01:54:27 -0400

    Small fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,8 +26,8 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   width: 'device-width',
   initialScale: 1,
-  maximumScale: 1,
-  userScalable: false,
+  maximumScale: 5,
+  userScalable: true,
   viewportFit: 'cover'
 };
 
